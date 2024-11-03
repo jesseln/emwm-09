9c8d3eb3d6c564602c0de72ca31a0e5bad7191f0
jesseln <48170789+jesseln@users.noreply.github.com>
2024-11-03 18:05:51 +1100

    augmented nuxt.config file to include previous settings. Copied assets and removed tailwind and sass. Copied components, composables, pages, plugins, stores from previous project as is.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,5 +18,48 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     'floating-vue/nuxt',
     '@nuxtjs/stylelint-module',
-  ]
+  ],
+  image: {
+    presets: {
+      bookImage: {
+        modifiers: {
+          width: 200,
+          height: 200,
+        }
+      }
+    }
+  },
+  googleFonts: {
+    families: {
+      // a name with spaces
+      Roboto: true,
+      'Source Sans 3': [100,150,200,250,300,350,400,450,500,550,600,650,700],
+      'Source Serif 4': [100,150,200,250,300,350,400,450,500,550,600,650,700],
+      Raleway: [100,150,200,250,300,350,400,450,500,550,600,650,700],
+      // specific font weights
+      // Lato: [100, 300],
+    }
+  },
+  css: [
+    // CSS file in the project
+    '@/assets/css/main.css',
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" 
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css' 
+        },
+        // {
+        //   rel: 'stylesheet',
+        //   href: 'https://fonts.googleapis.com/css2?family=Raleway:wght@100;400&display=swap'
+        // }
+      ] 
+    }
+  }
 })
